a3ca1db2e67194f79bf9835e2dfb4dde97f0d8bd
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-02-07 18:33:31 -0300

    Style added, front end socket connection done

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,20 @@ const socket = io.connect("http://localhost:3000")
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
   const joinRoom = () =>{
+
     if(username !== "" && room !==""){
-      console.log(room)
-      socket.emit("join_room" , room);
-
+    socket.emit("join_room" , room);
+    setShowChat(true);
+    
     }
-
   };
   return (
     <div className="App">
-      
+      {!showChat ? (
+      <div className='joinChatContainer'>
       <h3>Join a chat</h3>
       <input 
       type="text"
@@ -31,12 +34,14 @@ function App() {
       }}
         />
       <button onClick={joinRoom}>Join A Room</button>
-
+      </div>
+   ) : (
       <Chat 
         socket={socket} 
         username={username}
         room={room}
       />
+      )}
     </div>
   );
 }
